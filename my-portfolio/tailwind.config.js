ebdbfc7a040e092d58daef9abfd725550e1127d4
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2024-06-02 13:09:49 +0530

    initial setup done

---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primarycolor:'#8873ef',
+        headingColor:'#081e21',
+        smallTextColor:'#193256',
+      }
+    },
+  },
+  plugins: [],
+}
